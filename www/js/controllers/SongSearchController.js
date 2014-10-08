d980322828f17f7c5899eb267dcd481ce288635d
Remo Raeber <remo@raeberus.ch>
2014-10-08 17:01:20 +0200

    UI Cleanup: Tabs are not needed. Put controllers/services into dedicated sub-directories

---
+++ www/js/controllers/SongSearchController.js
@@ -0,0 +1,10 @@
+/*
+ * View controller for the searchable song list.
+ *
+ * Note: Defines a new controller on top of the existing module.
+ *  - Make sure to include this file after loading the 'songbook' module
+ */
+angular.module("songbook")
+    .controller("SongSearchController", function($scope) {
+
+    });
