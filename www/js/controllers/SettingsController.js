65be123d4d45cc97ac47956da714af01e99b7c67
Remo Raeber <remo@raeberus.ch>
2014-10-29 19:16:56 +0100

    Draft for settings controller with scroll demo

--- www/js/controllers/SettingsController.js
+++ www/js/controllers/SettingsController.js
@@ -1,6 +1,37 @@
 /**
  * View controller for the (modal) settings view.
  */
-Songbook.controller("SettingsController", function($scope) {
+Songbook.controller("SettingsController", function($scope, $ionicScrollDelegate) {
+    var SCROLL_STEP = 5;
+    $scope.scroll = {};
+    $scope.scroll.enabled = false;
+    $scope.scroll.speed = 1;
 
+    var scrollHandle = $ionicScrollDelegate.$getByHandle("scrollDemo");
+
+    var direction = 1;
+    var lastPosition = -1;
+    var changedDirection = false;
+    var doScroll = function() {
+        if ($scope.scroll.enabled) {
+            scrollHandle.scrollBy(0, $scope.scroll.speed * SCROLL_STEP * direction, true);
+
+            var currentPosition = scrollHandle.getScrollPosition().top;
+            if (!changedDirection && Math.ceil(Math.abs(lastPosition * 10 - currentPosition * 10)) < 5) {
+                direction *= -1;
+                changedDirection = true;
+            } else {
+                changedDirection = false;
+            }
+            lastPosition = currentPosition;
+
+            setTimeout(doScroll, 100);
+        }
+    };
+
+    $scope.onScrollToggled = function() {
+        if ($scope.scroll.enabled) {
+            doScroll();
+        }
+    };
 });
