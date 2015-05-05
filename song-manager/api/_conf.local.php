63f3e319b230c06928502947265fc7e449997b14
Demian Holderegger <demian.holderegger@gmail.com>
2015-05-05 20:15:04 +0200

    Adding Doctrine to API

---
+++ song-manager/api/_conf.local.php
@@ -0,0 +1,11 @@
+<?php
+
+$SQL_CREDENTIALS = array(
+	'dbname' => 'song-manager',
+	'user' => 'root',
+	'password' => '',
+	'host' => '127.0.0.1',
+	'driver' => 'pdo_mysql',
+);
+
+date_default_timezone_set('Europe/Zurich');
--- song-manager/api/index.php
+++ song-manager/api/index.php
@@ -1,14 +1,22 @@
 <?php
+require '_conf.php';
 require '../vendor/autoload.php';
+
+use Doctrine\DBAL\DriverManager;
+
+$DB = DriverManager::getConnection($SQL_CREDENTIALS, new \Doctrine\DBAL\Configuration());
+
 
 $app = new \Slim\Slim();
 
-$app->get('/songs', function () {
-	echo json_encode(array('bla','blub'));
+$app->get('/songs', function () use(&$DB) {
+	$songs = $DB->fetchAll("SELECT * FROM songs");
+	echo json_encode($songs);
 });
 
-$app->get('/songs/:songId', function ($songId) {
-	echo json_encode(array($songId,'bla','blub'));
+$app->get('/songs/:songId', function ($songId) use(&$DB) {
+	$song = $DB->fetchAssoc("SELECT * FROM songs WHERE id = ?", array($songId));
+	echo json_encode($song);
 });
 
 $app->run();
