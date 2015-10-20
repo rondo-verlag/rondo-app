a2e83288fda7cd728c165cf55971fb2c2bb28d80
Demian Holderegger <demian.holderegger@gmail.com>
2015-10-20 09:39:48 +0200

    fix äöü uppercase convert

--- song-manager/api/index.php
+++ song-manager/api/index.php
@@ -227,7 +227,7 @@ $app->get('/export/index', function () use ($app, &$DB) {
 
 	foreach($songs as $song){
 		$alternativeTitles = $song['alternativeTitles'];
-		$song['title'] = strtoupper(normalizer_normalize($song['title'], Normalizer::FORM_C ));
+		$song['title'] = strtoupper(normalizer_normalize($song['title'], Normalizer::FORM_KD));
 		unset($song['alternativeTitles']);
 		$index[] = $song;
 
