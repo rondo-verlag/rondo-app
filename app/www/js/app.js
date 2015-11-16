fb4c146f145a57c9ea34d4168ba2b14e3e237f9e
Demian Holderegger <demian.holderegger@gmail.com>
2015-11-16 20:55:25 +0100

    remove analytics

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -1,13 +1,12 @@
-var Songbook = angular.module("songbook", ['ionic','ionic.service.core', 'ionic.service.analytics','ionic.service.core', 'ui.router', 'ngCordova', 'ngStorage', 'hmTouchEvents']);
+var Songbook = angular.module("songbook", ['ionic', 'ui.router', 'ngCordova', 'ngStorage', 'hmTouchEvents']);
 
-Songbook.run(function ($ionicPlatform, $ionicAnalytics) {
+Songbook.run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
-        $ionicAnalytics.register();
     });
 })
 
