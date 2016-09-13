8f76bf887779d9aa44ca7b09716e798460b1ab4c
Demian Holderegger <demian.holderegger@gmail.com>
2016-09-13 23:35:05 +0200

    add chord playback

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -75,13 +75,33 @@ Songbook.controller("AppController", function ($scope) {
  * View controller for the chord list page.
  *
  */
-Songbook.controller("ChordListController", function ($scope, $rootScope, $document, $stateParams, $http, SongService) {
+Songbook.controller("ChordListController", function ($scope, $rootScope, $document, $stateParams, $http, SongService, $ionicPlatform) {
     $scope.songId = $stateParams.songId;
     $scope.data = [];
     SongService.getSongInfo($scope.songId)
         .then(function (data) {
         $scope.data = data;
         addFBarChordIfNeeded($scope.data.chords);
+    });
+    $ionicPlatform.ready(function () {
+        var media_path = "/android_asset/www/resources/songs/mp3-chords/";
+        var media;
+        $scope.playMedia = function (chord) {
+            var src = media_path + chord + '.mp3';
+            if (media) {
+                media.stop();
+                media.release();
+            }
+            media = new Media(src, function () { }, function (error) {
+                console.log('MEDIA error: ' + error.code);
+                media.release();
+                media = null;
+            });
+            media.play();
+        };
+        $scope.$on('destroy', function () {
+            media.release();
+        });
     });
     function addFBarChordIfNeeded(chords) {
         var fBarIndex = chords.indexOf("F-bar");
--- app/www/js/controllers/ChordListController.ts
+++ app/www/js/controllers/ChordListController.ts
@@ -2,7 +2,7 @@
  * View controller for the chord list page.
  *
  */
-Songbook.controller("ChordListController", function ($scope, $rootScope, $document, $stateParams, $http, SongService) {
+Songbook.controller("ChordListController", function ($scope, $rootScope, $document, $stateParams, $http, SongService, $ionicPlatform) {
   $scope.songId = $stateParams.songId;
   $scope.data = [];
 
@@ -11,6 +11,33 @@ Songbook.controller("ChordListController", function ($scope, $rootScope, $docume
         $scope.data = data;
         addFBarChordIfNeeded($scope.data.chords);
       });
+
+  $ionicPlatform.ready(function() {
+      var media_path = "/android_asset/www/resources/songs/mp3-chords/";
+      var media;
+
+      $scope.playMedia = function(chord: string) {
+        var src = media_path + chord + '.mp3';
+        if (media) {
+          media.stop();
+          media.release();
+        }
+        media = new Media(src,
+          () => {},
+          (error) => {
+            console.log('MEDIA error: ' + error.code);
+            media.release();
+            media = null;
+          }
+        );
+        media.play();
+      };
+
+      $scope.$on('destroy', function() {
+        media.release();
+      });
+
+   });
 
   function addFBarChordIfNeeded(chords){
     var fBarIndex = chords.indexOf("F-bar");
