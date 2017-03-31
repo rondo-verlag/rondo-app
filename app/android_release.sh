cfceb2f2e0aeef6453f851d6306ebc0b192d8fca
Demian Holderegger <demian.holderegger@gmail.com>
2017-03-31 16:28:57 +0200

    more stable build script

--- app/android_release.sh
+++ app/android_release.sh
@@ -2,4 +2,5 @@
 
 cordova build --release android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore keystore/android-release-key.keystore /Users/demian/projects/demianh/songbook-app/app/platforms/android/build/outputs/apk/android-release-unsigned.apk rondoapp
-rm build/Rondo.apk && ~/Library/Android/sdk/build-tools/23.0.2/zipalign -v 4 /Users/demian/projects/demianh/songbook-app/app/platforms/android/build/outputs/apk/android-release-unsigned.apk build/Rondo.apk
+rm build/Rondo.apk
+~/Library/Android/sdk/build-tools/23.0.2/zipalign -v 4 /Users/demian/projects/demianh/songbook-app/app/platforms/android/build/outputs/apk/android-release-unsigned.apk build/Rondo.apk
