d9f141210054926957f3ccac5a56b5ff94b1448b
Demian Holderegger <demian.holderegger@gmail.com>
2017-09-28 10:30:29 +0200

    black theme and song listing

--- app_v2/songbook-app/src/pages/home/home.ts
+++ app_v2/songbook-app/src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SongIndexServiceProvider } from "../../providers/song-index-service/song-index-service";
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [SongIndexServiceProvider]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private songs = [];
 
+  constructor(public navCtrl: NavController, public songIndexService: SongIndexServiceProvider) {
+    this.loadSongs();
+  }
+
+  loadSongs(){
+    this.songIndexService.load()
+    .then(data => {
+      this.songs = data;
+    });
   }
 
 }
