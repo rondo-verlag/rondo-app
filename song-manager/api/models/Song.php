e52fabb4e79213f1c5b79c572c2f94a8ec60d0d3
Demian Holderegger <demian.holderegger@gmail.com>
2018-03-11 15:00:44 +0100

    make chordlist match the classnames

--- song-manager/api/models/Song.php
+++ song-manager/api/models/Song.php
@@ -279,7 +279,7 @@ class Song
 		$uncleaned_chords = $this->getChordList();
 		$cleaned = [];
 		foreach ($uncleaned_chords as $chord) {
-			$cleaned[] = preg_replace("/[^A-Za-z0-9\/]/", '', $chord);
+			$cleaned[] = str_replace('/', '-', preg_replace("/[^A-Za-z0-9\/]/", '', $chord));
 		}
 		$list = array_unique($cleaned);
 		sort($list);
