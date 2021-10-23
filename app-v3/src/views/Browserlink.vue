55588f68818230e17ff57379ca4a26ea44dec6a1
Demian Holderegger <demian.holderegger@gmail.com>
2021-10-24 01:55:07 +0200

    implement songtext with scrolling

---
+++ app-v3/src/views/Browserlink.vue
@@ -0,0 +1,17 @@
+<template>
+  <a :href="url" target="_blank"><slot></slot></a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Browserlink',
+  props: {
+    url: String
+  },
+});
+</script>
+
+<style scoped>
+</style>
