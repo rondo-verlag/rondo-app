9a818b38a2d27b73310d3ecee3ca3ca6aa644031
Demian Holderegger <demian.holderegger@gmail.com>
2021-10-24 02:00:15 +0200

    update configs

--- app-v3/capacitor.config.ts
+++ app-v3/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'ch.rondo.songbookapp',
   appName: 'songbook-app',
   webDir: 'dist',
   bundledWebRuntime: false
