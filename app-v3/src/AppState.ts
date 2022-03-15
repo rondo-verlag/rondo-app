77665b366879d540abf7452a6b4ac59557293824
Demian Holderegger <demian.holderegger@gmail.com>
2022-03-15 23:16:17 +0100

    implement persistent storage

--- app-v3/src/AppState.ts
+++ app-v3/src/AppState.ts
@@ -1,5 +1,13 @@
 import { reactive } from "vue";
+import { GetResult, Storage } from '@capacitor/storage';
 
-export default reactive({
+let state = reactive({
     hasBought: false
 });
+
+// load initial state from persistent storage
+Storage.get({ key: 'hasBought' }).then((result: GetResult) => {
+    state.hasBought = result.value == 'true';
+});
+
+export default state;
