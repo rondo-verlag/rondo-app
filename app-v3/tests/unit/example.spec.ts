9cfa19df4a81f02b5ca3c7502020c5952b51dac7
Demian Holderegger <demian.holderegger@gmail.com>
2022-03-15 21:53:11 +0100

    upgrade other dependencies

--- app-v3/tests/unit/example.spec.ts
+++ app-v3/tests/unit/example.spec.ts
@@ -1,9 +1,12 @@
-import { mount } from '@vue/test-utils'
-import Home from '@/views/Home.vue'
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('Home.vue', () => {
-  it('renders home vue', () => {
-    const wrapper = mount(Home)
-    expect(wrapper.text()).toMatch('Ready to create an app?')
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
   })
 })
