7569157ffa44455f60a9f9cfbb2bb1492292c171
Demian Holderegger <demian.holderegger@gmail.com>
2022-03-15 21:53:11 +0100

    upgrade other dependencies

--- app-v3/.eslintrc.js
+++ app-v3/.eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     'prefer-const': 'off',
+    'vue/multi-word-component-names': 'warn'
   },
   overrides: [
     {
