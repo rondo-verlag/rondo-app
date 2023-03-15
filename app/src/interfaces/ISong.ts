bf9eec8fc50de431ed0334349097a43a3cce5c51
Vento <github@brenneisen.me>
2023-03-15 20:09:15 +0100

    Update ISong.ts

--- app/src/interfaces/ISong.ts
+++ app/src/interfaces/ISong.ts
@@ -1,14 +1,14 @@
 export default interface ISong {
-    id: number;
-    title: string;
-    pageRondoRed: number|null;
-    pageRondoBlue: number|null;
-    pageRondoGreen: number|null;
-    pageRondo2017: number|null;
-    pageRondo2021: number|null;
-    interpret: string;
-    chords: string[];
-    alternative: boolean;
-    free: boolean;
-    license: string;
+  id: number;
+  title: string;
+  pageRondoRed: number|null;
+  pageRondoBlue: number|null;
+  pageRondoGreen: number|null;
+  pageRondo2017: number|null;
+  pageRondo2021: number|null;
+  interpret: string;
+  chords: string[];
+  alternative: boolean;
+  free: boolean;
+  license: string;
 }
