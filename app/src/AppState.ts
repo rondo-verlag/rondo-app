bf4e3626ac269257a9977cc4d046cdcb8ea37515
Vento <github@brenneisen.me>
2023-03-19 16:05:05 +0100

    Update AppState.ts

--- app/src/AppState.ts
+++ app/src/AppState.ts
@@ -2,17 +2,17 @@ import { reactive } from "vue";
 import { GetResult, Storage } from '@capacitor/storage';
 
 let state = reactive({
-    hasBought: false,
-    setHasBought: (val: boolean) => {
-        state.hasBought = val;
-        // save value in persistent storage
-        Storage.set({ key: 'hasBought', value: val ? 'true' : 'false' });
-    }
+  hasBought: false,
+  setHasBought: (val: boolean) => {
+    state.hasBought = val;
+    // save value in persistent storage
+    Storage.set({ key: 'hasBought', value: val ? 'true' : 'false' });
+  }
 });
 
 // load initial state from persistent storage
 Storage.get({ key: 'hasBought' }).then((result: GetResult) => {
-    state.hasBought = result.value == 'true';
+  state.hasBought = result.value == 'true';
 });
 
 export default state;
