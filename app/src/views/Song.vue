8535262a4363d18b0fa5ceb4ca7d35bd0d1ee428
Demian Holderegger <demian.holderegger@gmail.com>
2024-01-14 11:41:14 +0100

    do not throw if currentSong is null

--- app/src/views/Song.vue
+++ app/src/views/Song.vue
@@ -8,7 +8,7 @@
           </ion-button>
         </ion-buttons>
         <ion-buttons slot="end" class="rondo-header-buttons-right" v-if="section === 'text'">
-          <ion-button ion-button :href="currentSong.links.youtube" v-if="currentSong.links && currentSong.links.youtube">
+          <ion-button ion-button :href="currentSong.links.youtube" v-if="currentSong && currentSong.links && currentSong.links.youtube">
             <i class="icon rondo-icon-youtube"></i>
           </ion-button>
           <ion-button ion-button @click="toggleChords()">
