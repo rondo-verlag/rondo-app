1569424edb435b859372e4c4d5695a9a0c5a263e
Vento <github@brenneisen.me>
2026-09-12 20:51:31 +0200

    Implement leave app with back button

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,5 +5,26 @@
 </template>
 
 <script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { IonApp, IonRouterOutlet, toastController, useBackButton, useIonRouter } from '@ionic/vue';
+import { App } from '@capacitor/app';
+
+const ionRouter = useIonRouter();
+let lastBackPress = 0;
+
+useBackButton(-1, async () => {
+  if (!ionRouter.canGoBack()) {
+    const now = Date.now();
+    if (now - lastBackPress < 2000) {
+      await App.exitApp();
+    } else {
+      lastBackPress = now;
+      const toast = await toastController.create({
+        message: 'Zum Beenden noch einmal tippen',
+        duration: 2000,
+        position: 'bottom',
+      });
+      await toast.present();
+    }
+  }
+});
 </script>
