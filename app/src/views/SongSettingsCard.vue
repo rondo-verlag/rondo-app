2d944a2dbb6f0ff1c472b76516b0e74f6a5c7376
Vento <github@brenneisen.me>
2026-09-12 18:27:01 +0200

    Settings pages

--- app/src/views/SongSettingsCard.vue
+++ app/src/views/SongSettingsCard.vue
@@ -1,10 +1,44 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-button @click="$router.back()" class="rondo-back-button">
+            <i class="icon rondo-icon-arrow"></i>
+          </ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
 
+    <ion-content :fullscreen="true">
+
+    </ion-content>
+  </ion-page>
 </template>
+
+<script lang="ts">
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonToolbar,
+} from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SongSettingsCard',
+  components: {
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonToolbar,
+  },
+})
+</script>
 
 <style scoped lang="scss">
 
--- app/src/views/ViewSettingsCard.vue
+++ app/src/views/ViewSettingsCard.vue
@@ -1,10 +1,48 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-button @click="$router.back()" class="rondo-back-button">
+            <i class="icon rondo-icon-arrow"></i>
+          </ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
 
+    <ion-content :fullscreen="true">
+      <div class="container">
+        <h1>Anzeigeeinstellungen</h1>
+
+
+      </div>
+    </ion-content>
+  </ion-page>
 </template>
+
+<script lang="ts">
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonToolbar,
+} from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ViewSettingsCard',
+  components: {
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonToolbar,
+  },
+})
+</script>
 
 <style scoped lang="scss">
 
