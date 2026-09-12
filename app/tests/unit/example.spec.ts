539f44ad85861162e6af07c75bfab3c60f5b4c17
Vento <github@brenneisen.me>
2026-09-12 12:56:09 +0200

    Update cypress config

--- app/tests/unit/example.spec.ts
+++ app/tests/unit/example.spec.ts
@@ -1,12 +1,10 @@
 import { mount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomePage from '@/views/HomePage.vue'
+import { describe, expect, test } from 'vitest'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = mount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('HomePage.vue', () => {
+  test('renders home view', () => {
+    const wrapper = mount(HomePage)
+    expect(wrapper.text()).toMatch('Inbox')
   })
 })
