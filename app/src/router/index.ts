e02d3b724cd4f742b7503de90c7d42c0030bc636
Vento <github@brenneisen.me>
2026-09-12 18:07:24 +0200

    Add new routes

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,9 +1,12 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import SongList from "@/views/SongList.vue";
-import About from "@/views/About.vue";
 import SongCard from "@/views/SongCard.vue";
 import MenuList from "@/views/MenuList.vue";
+import PaymentCard from "@/views/PaymentCard.vue";
+import SongSettingsCard from "@/views/SongSettingsCard.vue";
+import ViewSettingsCard from "@/views/ViewSettingsCard.vue";
+import AboutCard from "@/views/AboutCard.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -25,6 +28,26 @@ const routes: Array<RouteRecordRaw> = [
     name: 'MenuList',
     component: MenuList
   },
+  {
+    path: '/payment',
+    name: 'PaymentCard',
+    component: PaymentCard
+  },
+  {
+    path: '/song-settings',
+    name: 'SongSettingsCard',
+    component: SongSettingsCard
+  },
+  {
+    path: '/view-settings',
+    name: 'ViewSettingsCard',
+    component: ViewSettingsCard
+  },
+  {
+    path: '/about',
+    name: 'AboutCard',
+    component: AboutCard
+  }
 ]
 
 const router = createRouter({
