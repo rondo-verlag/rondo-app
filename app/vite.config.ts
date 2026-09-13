17b92fdd422a8774c2377fa98e15846307fd68d9
Vento <github@brenneisen.me>
2026-09-13 12:29:57 +0200

    Remove legacy vue support

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,15 +1,13 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    legacy()
+    vue()
   ],
   worker: {
     format: 'iife',
@@ -27,4 +25,4 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom'
   }
-})
+});
